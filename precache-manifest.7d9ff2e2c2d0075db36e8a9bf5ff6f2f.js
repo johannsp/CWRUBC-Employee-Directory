4f0e16bbb4561775e4eee55f9953281a18aa2ebf
John L Spetz <JohnLSpetz@zoho.com>
2020-10-10 21:47:51 -0400

    Updates

---
+++ precache-manifest.7d9ff2e2c2d0075db36e8a9bf5ff6f2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6466b8299770aaa45777e14d37b65009",
+    "url": "/CWRUBC-Employee-Directory/index.html"
+  },
+  {
+    "revision": "154518cca07350d1489e",
+    "url": "/CWRUBC-Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "608d9013e9fd6110bb03",
+    "url": "/CWRUBC-Employee-Directory/static/css/main.03391ff9.chunk.css"
+  },
+  {
+    "revision": "154518cca07350d1489e",
+    "url": "/CWRUBC-Employee-Directory/static/js/2.36d84d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CWRUBC-Employee-Directory/static/js/2.36d84d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608d9013e9fd6110bb03",
+    "url": "/CWRUBC-Employee-Directory/static/js/main.0a8505a2.chunk.js"
+  },
+  {
+    "revision": "d03d8ee0019fce511509",
+    "url": "/CWRUBC-Employee-Directory/static/js/runtime-main.997ad94b.js"
+  }
+]);
